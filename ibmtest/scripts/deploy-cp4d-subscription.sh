bfe60361fa7a6726055b2a18eb81bed3d88163be
LShort-IBM <Lance.Short1@ibm.com>
2026-07-12 18:21:11 -0400

    change operator pod deployment name

--- ibmtest/scripts/deploy-cp4d-subscription.sh
+++ ibmtest/scripts/deploy-cp4d-subscription.sh
@@ -54,15 +54,15 @@ echo "=========================================="
 echo "Ensuring Operator Pod is Fully Ready..."
 echo "=========================================="
 # Step 1: Wait for OLM to actually create the deployment object
-while ! oc get deployment ibmcpd-operator -n cp4d-operators --kubeconfig=$KUBECONFIG >/dev/null 2>&1; do
+while ! oc get deployment cpd-platform-operator-manager -n cp4d-operators --kubeconfig=$KUBECONFIG >/dev/null 2>&1; do
   echo "Waiting for the deployment object... (checking again in 15s)"
   sleep 15
 done
 
 # Step 2: Wait for the pod to become fully Ready
 echo "Operator deployment found! Waiting for the pod to pass health checks..."
-oc rollout status deployment/ibmcpd-operator -n cp4d-operators --kubeconfig=$KUBECONFIG --timeout=5m
+oc rollout status deployment/cpd-platform-operator-manager -n cp4d-operators --kubeconfig=$KUBECONFIG --timeout=5m
 
 # Step 3: Buffer for internal Ansible runner webhooks to settle
-echo "Operator is Ready! Letting webhooks settle for 15 seconds..."
+echo "Operator is Ready! Letting webhooks settle for 60 seconds..."
 sleep 60
