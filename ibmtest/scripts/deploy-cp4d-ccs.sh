29f9fd2ebf6dab47c41877602c939cded939ca38
LShort-IBM <Lance.Short1@ibm.com>
2026-07-14 14:43:54 -0400

    add ccs install

--- ibmtest/scripts/deploy-cp4d-ccs.sh
+++ ibmtest/scripts/deploy-cp4d-ccs.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
+set -e
+
+# Tell cpd-cli how to authenticate with the cluster
+export KUBECONFIG="/home/itzuser/kubeconfig"
+
+CPD_VERSION="4.4.0"
+OPERATOR_NS="cp4d-operators"
+INSTANCE_NS="cpd"
 
 echo "Authorizing instance topology..."
 cpd-cli manage authorize-instance-topology \
-  --cpd_operator_ns=cp4d-operators \
-  --cpd_instance_ns=cpd
+  --cpd_operator_ns=${OPERATOR_NS} \
+  --cpd_instance_ns=${INSTANCE_NS}
 
 echo "Installing CCS Operator (OLM)..."
 cpd-cli manage apply-olm \
-  --release=4.4.0 \
-  --cpd_operator_ns=cp4d-operators \
+  --release=${CPD_VERSION} \
+  --cpd_operator_ns=${OPERATOR_NS} \
   --components=ccs
 
 echo "Deploying CCS Custom Resource..."
 cpd-cli manage apply-cr \
-  --release=4.4.0 \
-  --cpd_instance_ns=cpd \
+  --release=${CPD_VERSION} \
+  --cpd_instance_ns=${INSTANCE_NS} \
   --components=ccs \
   --license_acceptance=true
