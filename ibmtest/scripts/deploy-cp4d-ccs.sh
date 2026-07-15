3a9ce40676be349aaab2b0b346fc9749be1fdff0
LShort-IBM <Lance.Short1@ibm.com>
2026-07-15 10:35:45 -0400

    remove topology step

--- ibmtest/scripts/deploy-cp4d-ccs.sh
+++ ibmtest/scripts/deploy-cp4d-ccs.sh
@@ -1,34 +1,28 @@
 #!/bin/bash
+#!/bin/bash
 set -e
-export KUBECONFIG="${HOME}/.kube/config"
 
-# Force cpd-cli to use the CPD 4.x compatible utility image
-export OLM_UTILS_IMAGE="icr.io/cpopen/cpd/olm-utils-v2:latest"
+# Use the v1 container which holds the 4.4.0 playbooks
+export OLM_UTILS_IMAGE="icr.io/cpopen/cpd/olm-utils:latest"
 
-CPD_VERSION="6.4.7"
+CPD_VERSION="4.4.0"
 OPERATOR_NS="cpd-operators"
 INSTANCE_NS="cpd"
 
 echo "----------------------------------------------------"
-echo "1. Authorizing instance topology..."
-echo "----------------------------------------------------"
-cpd-cli manage authorize-instance-topology \
-  --cpd_operator_ns=${OPERATOR_NS} \
-  --cpd_instance_ns=${INSTANCE_NS}
-
-echo "----------------------------------------------------"
-echo "2. Installing CCS Operator (OLM)..."
+echo "1. Installing CCS Operator (OLM)..."
 echo "----------------------------------------------------"
 cpd-cli manage apply-olm \
-  --release=${CPD_VERSION} \
-  --cpd_operator_ns=${OPERATOR_NS} \
-  --components=ccs
+  --release="${CPD_VERSION}" \
+  --cpd_operator_ns="${OPERATOR_NS}" \
+  --components=ccs \
+  --license_acceptance=true
 
 echo "----------------------------------------------------"
-echo "3. Deploying CCS Custom Resource..."
+echo "2. Deploying CCS Custom Resource..."
 echo "----------------------------------------------------"
 cpd-cli manage apply-cr \
-  --release=${CPD_VERSION} \
-  --cpd_instance_ns=${INSTANCE_NS} \
+  --release="${CPD_VERSION}" \
+  --cpd_instance_ns="${INSTANCE_NS}" \
   --components=ccs \
   --license_acceptance=true
